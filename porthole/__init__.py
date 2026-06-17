2f42c2a1a00f6935bc43780c43f118a29c09b98f
Joe Black <jrb00013wvu@gmail.com>
2026-06-17 12:35:55 -0400

    chore: expose package metadata from __init__

--- porthole/__init__.py
+++ porthole/__init__.py
@@ -1,3 +1,3 @@
-from .core import __version__
+from .core import __version__, __title__, __description__, __acronym__, __acronym_full__
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "__title__", "__description__", "__acronym__", "__acronym_full__"]
